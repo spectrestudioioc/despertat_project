4a559499f235ccfac57a9e9e6701c6d8a9b995e5
spectrestudioioc <spectrestudioioc@gmail.com>
2025-03-12 17:04:53 +0100

    snapshot-1

---
+++ Assets/EasyStart Third Person Controller/Scripts/PickupController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    private GameManager gameManager; // Referència al GameManager
+
+    public bool areaPickup;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance; // Obtenim la instància del GameManager
+        areaPickup = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Comprova si el GameObject que toca és el Player
+        {
+            Debug.Log("El player ha entrat al trigger del pickup!");
+            gameManager.MostraPickupText(this);
+            areaPickup = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Comprova si el GameObject que toca és el Player
+        {
+            Debug.Log("El player ha sortit del trigger del pickup!");
+            gameManager.AmagaPickupText(this);
+            areaPickup = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (areaPickup && Input.GetKeyDown(KeyCode.E)) // Comprovem si el jugador està dins del trigger i prem la tecla E
+        {
+            Debug.Log("El jugador ha clicat la tecla E davant del pickup");
+            gameManager.RecullPickup(this);
+        }
+    }
+}
