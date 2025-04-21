6f64a0db0aa4dd871e1f65abf22f6c351e9db28c
Josep <josemarinmolero@gmail.com>
2025-04-21 11:26:05 +0200

    GUI_Transitions

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public enum TipusEnemic
+    {
+        EnemicOmbra,
+        EnemicRialla,
+        EnemicGranPena
+    }
+
+    public TipusEnemic tipusEnemic;
+    public int dany;
+
+    private PlayerHealth playerHealth; // Referencia a un component PlayerHealth
+
+    private void Start()
+    {
+        // Obtenim la referència al component PlayerHealth
+        playerHealth = FindObjectOfType<PlayerHealth>();
+
+        switch (tipusEnemic)
+        {
+            case TipusEnemic.EnemicOmbra:
+                dany = 10;
+                break;
+            case TipusEnemic.EnemicRialla:
+                dany = 15;
+                break;
+            case TipusEnemic.EnemicGranPena:
+                dany = 20;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void TakeDamage(EnemyCollider enemyCollider)
+    {
+        
+        if (playerHealth != null)
+        {
+
+            playerHealth.TakeDamage(dany); // Restem la vida al jugador
+            // Mostrem un missatge a la consola cada vegada que apliquem dany
+            Debug.Log($"S'ha aplicat {dany} de dany al jugador. Salut restant: {playerHealth.currentHealth}");
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; // Singleton per accedir fàcilment al GameManager
+    public TextMeshProUGUI pickupText;
+
+    public Slider healthSlider; 
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void MostraPickupText(PickupController pickup)
+    {
+        pickupText.gameObject.SetActive(true); // Assegurem que el text estigui visible
+        pickupText.text = "Prem la tecla E per recollir"; // Mostrem el missatge per recollir
+    }
+
+    public void AmagaPickupText(PickupController pickup)
+    {
+        pickupText.gameObject.SetActive(false); // Assegurem que el text ja no sigui visible       
+    }
+
+    public void RecullPickup(PickupController pickup)
+    {
+        AmagaPickupText(pickup);
+        pickup.gameObject.SetActive(false);
+    }
+
+
+}
+
